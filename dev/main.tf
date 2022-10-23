c291f5ea2fa0fcd585e5d803ab3855550abb2b71
Daniel Curilla <dan.curilla@hootsuite.com>
2022-10-22 21:43:44 -0600

    simplify bucket name

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
 module "lifecycle_bucket" {
   source      = "../modules/lifecycle_bucket"
-  bucket_name = "dtc-nyc-taxi-${var.env}-${var.project_id}"
+  bucket_name = "nyc-taxi-${var.env}-${var.project_id}"
   region      = var.region
   max_age     = 30 // days
 }
