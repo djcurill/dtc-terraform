d1ec25f8d107b9464cfa10fbd4a66e85888d0fdb
Daniel Curilla <dan.curilla@hootsuite.com>
2022-11-01 13:50:13 -0600

    change name of subnetwork

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "cloud_composer_subnet" {
-  name          = "cloud_composer_subnet-${var.env}"
+  name          = "cloud-composer-subnet-${var.env}"
   ip_cidr_range = "10.2.0.0/16"
   region        = var.region
   network       = google_compute_network.vpc.id
