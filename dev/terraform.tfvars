4969ffe94e4c61c29ece2e9c41990c8d42de56d8
Daniel Curilla <dan.curilla@hootsuite.com>
2022-11-03 19:25:10 -0600

    attempting to create cloud composer env

--- dev/terraform.tfvars
+++ dev/terraform.tfvars
@@ -1,5 +1,6 @@
-project_id = "dtc-dataops-dev-366316"
-region     = "us-west1"
-env        = "dev"
-BQ_DATASET = "trips_data_all"
+project_id     = "dtc-dataops-dev-366316"
+region         = "us-west1"
+env            = "dev"
+BQ_DATASET     = "trips_data_all"
+project_number = 523776208646
 
---
+++ modules/cloud_composer/main.tf
@@ -0,0 +1,59 @@
+resource "google_project_service" "composer_api" {
+  project            = var.project_id
+  service            = "composer.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_service_account" "cloud_composer_service_account" {
+  account_id   = "composer-service-account-${var.env}"
+  display_name = "Cloud Composer Service Account"
+}
+
+resource "google_project_iam_member" "cloud_composer_iam" {
+  for_each = toset([
+    "roles/storage.objectAdmin",
+    "roles/bigquery.dataEditor",
+    "roles/iam.serviceAccountUser",
+    "roles/composer.worker"
+  ])
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.cloud_composer_service_account.email}"
+  project = var.project_id
+}
+
+resource "google_service_account_iam_member" "cloud_composer_iam_binding" {
+  service_account_id = google_project_iam_member.cloud_composer_iam.name
+  role               = "roles/composer.ServiceAgentV2Ext"
+  member             = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+}
+
+resource "google_compute_subnetwork" "cloud_composer_subnet" {
+  name          = "cloud-composer-subnet-${var.env}"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_composer_environment" "cloud_composer" {
+  name    = "dtc-cloud-composer-${var.env}"
+  region  = var.region
+  project = var.project_id
+  config {
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+
+    software_config {
+      image_version = "composer-2.0.30-airflow-2.3.3"
+      airflow_config_overrides = {
+        core-dags_are_paused_at_creation = "True"
+      }
+    }
+
+    node_config {
+      network         = var.vpc.id
+      subnetwork      = google_compute_subnetwork.cloud_composer_subnet.id
+      service_account = google_service_account.cloud_composer_service_account.name
+    }
+
+  }
+}
+
